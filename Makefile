5826bab46cd0f513998a06ba6948088410497559
John Ousterhout <ouster@cs.stanford.edu>
2021-07-19 14:54:40 -0700

    Added tcp_test application

--- Makefile
+++ Makefile
@@ -18,12 +18,15 @@ PROTOS_PATH = .
 PKG_CONFIG_PATH = /ouster/install/lib/pkgconfig
 export PKG_CONFIG_PATH
 
-all: test_client test_server
+all: test_client test_server tcp_test
 	
-test_client: test_client.o homa_transport.o homa_api.o test.grpc.pb.o test.pb.o
+test_client: test.grpc.pb.o test.pb.o test_client.o homa_transport.o homa_api.o
 	$(CXX) $^ $(LDFLAGS) -o $@
 	
-test_server: test_server.o homa_listener.o homa_api.o test.grpc.pb.o test.pb.o
+test_server: test.grpc.pb.o test.pb.o test_server.o homa_listener.o homa_api.o
+	$(CXX) $^ $(LDFLAGS) -o $@
+	
+tcp_test: test.grpc.pb.o test.pb.o tcp_test.o
 	$(CXX) $^ $(LDFLAGS) -o $@
 	
 homa_api.o: /users/ouster/homaModule/homa_api.c
@@ -35,7 +38,7 @@ clean:
 %.o: %.cc
 	$(CXX) -c $(CXXFLAGS) -std=c++17 $< -o $@
 
-%.grpc.pb.cc: %.proto
+%.grpc.pb.cc: %.proto %.pb.cc
 	$(PROTOC) -I $(PROTOS_PATH) --grpc_out=. --plugin=protoc-gen-grpc=$(GRPC_CPP_PLUGIN_PATH) $<
 
 %.pb.cc: %.proto
---
+++ tcp_test.cc
@@ -0,0 +1,89 @@
+// This file contains both the client and server code for a simple
+// gRPC application.
+//
+// Usage:
+// tcp_test server port
+// tcp test client port@host
+
+#include <string>
+
+#include <grpc/grpc.h>
+#include <grpcpp/grpcpp.h>
+
+#include "test.grpc.pb.h"
+
+// Server-side code to implement the service.
+class SumService : public test::Test::Service {   
+public:    
+    grpc::Status Sum(grpc::ServerContext*context, const test::SumArgs *args,
+            test::SumResult *result) {
+        result->set_sum(args->op1() + args->op2());
+        return grpc::Status::OK;
+    }
+};
+
+// Client-side wrapper class.
+class SumClient{
+public:
+    SumClient(const std::shared_ptr<grpc::Channel> channel)
+        : stub(test::Test::NewStub(channel)) {}
+        
+    std::unique_ptr<test::Test::Stub> stub;
+    
+    int Sum(int op1, int op2)
+    {
+        test::SumArgs args;
+        test::SumResult result;
+        grpc::ClientContext context;
+        
+        args.set_op1(op1);
+        args.set_op2(op2);
+        grpc::Status status = stub->Sum(&context, args, &result);
+        if (!status.ok()) {
+            printf("Sum RPC failed!\n");
+            return -1;
+        }
+        return result.sum();
+    }
+};
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        goto usage;
+    }
+    if (strcmp(argv[1], "client") == 0) {
+        std::string target = "node-1:4000";
+        if (argc == 3) {
+            target = argv[2];
+        } else if (argc != 2) {
+            goto usage;
+        }
+        SumClient client(grpc::CreateChannel(target,
+            grpc::InsecureChannelCredentials()));
+        int sum = client.Sum(22, 33);
+        printf("Sum of 22 and 33 is %d\n", sum);
+        return 0;
+    } else if (strcmp(argv[1], "server") == 0) {
+        std::string port = "0.0.0.0:4000";
+        if (argc == 3) {
+            port = "0.0.0.0:";
+            port += argv[2];
+        } else if (argc != 2) {
+            goto usage;
+        }
+        SumService service;
+        grpc::ServerBuilder builder;
+        builder.AddListeningPort(port, grpc::InsecureServerCredentials());
+        builder.RegisterService(&service);
+        std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+        if (server == nullptr)
+            exit(1);
+        server->Wait();
+    }
+    exit(0);
+
+usage:
+    fprintf(stderr, "Usage: %s client port@server or %s server port\n",
+            argv[0], argv[0]);
+    exit(1);
+}
